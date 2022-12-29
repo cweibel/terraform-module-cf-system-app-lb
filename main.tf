4a506bfedcaf2ae2504efa6a70e971173b8c3536
Chris Weibel <christopher.weibel@gmail.com>
2022-12-29 13:16:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,33 +8,29 @@ variable security_groups       {}  # Array of security groups to use
 variable apps_acm_arn          {}  # ACM arn for the apps certificates
 variable system_acm_arn        {}  # ACM arn for the system certificates
 variable internal_lb           { default = true } # Determine whether the load balancer is internal-only facing
-
 variable enable_route_53       { default = 1 }  # Disable if using CloudFlare or other DNS
-
-
-
 
 #################################################################################
 # ALB
 #################################################################################
-resource "aws_lb" "cf_system_app_alb" {
-  name               = "cf-system-apps-alb"
+resource "aws_lb" "cf_system_app_lb" {
+  name               = "cf-system-apps-lb"
   internal           = var.internal_lb
   load_balancer_type = "application"
   subnets            = var.subnet_ids
   security_groups    = var.security_groups
-  tags = merge({Name = "cf-system-apps-alb"}, var.resource_tags)
+  tags = merge({Name = "cf-system-apps-lb"}, var.resource_tags)
 }
 
 #################################################################################
 # ALB Target Group
 #################################################################################
-resource "aws_lb_target_group" "cf_system_app_alb_tg" {
-  name     = "cf-system-apps-alb-tg"
+resource "aws_lb_target_group" "cf_system_app_lb_tg" {
+  name     = "cf-system-apps-lb-tg"
   port     = 443
   protocol = "HTTPS"
   vpc_id   = var.vpc_id
-  tags     = merge({Name = "cf-system-apps-alb-tg"}, var.resource_tags)
+  tags     = merge({Name = "cf-system-apps-lb-tg"}, var.resource_tags)
   health_check {
     path = "/health"
     port = 8080
@@ -50,27 +46,27 @@ resource "aws_lb_target_group" "cf_system_app_alb_tg" {
 #     instance_group = "router"
 #   }
 # }
-# resource "aws_lb_target_group_attachment" "cf_system_app_alb_tga" {
+# resource "aws_lb_target_group_attachment" "cf_system_app_lb_tga" {
 #   count            = length(data.aws_instances.cf_router_instances.ids)
 #   target_id        = data.aws_instances.cf_router_instances.ids[count.index]
-#   target_group_arn = aws_lb_target_group.cf_system_app_alb_tg.arn
+#   target_group_arn = aws_lb_target_group.cf_system_app_lb_tg.arn
 #   port             = 443
 # }
 
 ################################################################################
 # ALB Listener - System Domain
 ################################################################################
-resource "aws_alb_listener" "cf_system_app_alb_listener" {
-  load_balancer_arn = aws_lb.cf_system_app_alb.arn
+resource "aws_alb_listener" "cf_system_app_lb_listener" {
+  load_balancer_arn = aws_lb.cf_system_app_lb.arn
   port = "443"
   protocol = "HTTPS"
   ssl_policy = "ELBSecurityPolicy-TLS-1-2-2017-01"
   certificate_arn = var.apps_acm_arn
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.cf_system_app_alb_tg.arn
+    target_group_arn = aws_lb_target_group.cf_system_app_lb_tg.arn
   }
-  tags = merge({Name = "cf-system-apps-alb-listener-sys"}, var.resource_tags)
+  tags = merge({Name = "cf-system-apps-lb-listener-sys"}, var.resource_tags)
 }
 
 
@@ -78,8 +74,8 @@ resource "aws_alb_listener" "cf_system_app_alb_listener" {
 # ALB Listener Certificate - Apps Domain
 # For future self - this is how you map a second domain to a listener
 ################################################################################
-resource "aws_alb_listener_certificate" "alb_listner_apps_crt" {
-  listener_arn    = aws_alb_listener.cf_system_app_alb_listener.arn
+resource "aws_alb_listener_certificate" "lb_listner_apps_crt" {
+  listener_arn    = aws_alb_listener.cf_system_app_lb_listener.arn
   certificate_arn = var.system_acm_arn
 }
 
@@ -87,29 +83,29 @@ resource "aws_alb_listener_certificate" "alb_listner_apps_crt" {
 ################################################################################
 # CF ALB Route53 DNS CNAME Record - System Domain
 ################################################################################
-resource "aws_route53_record" "cf_system_app_alb_record_sys" {
+resource "aws_route53_record" "cf_system_app_lb_record_sys" {
   count   = var.enable_route_53
   zone_id = var.route53_zone_id
   name    = var.system_domain
   type    = "CNAME"
   ttl     = "60"
-  records = ["${aws_lb.cf_system_app_alb.dns_name}"]
+  records = ["${aws_lb.cf_system_app_lb.dns_name}"]
 }
 
 ################################################################################
 # CF ALB Route53 DNS CNAME Record - Apps Domain
 ################################################################################
-resource "aws_route53_record" "cf_system_app_alb_record_apps" {
+resource "aws_route53_record" "cf_system_app_lb_record_apps" {
   count   = var.enable_route_53
   zone_id = var.route53_zone_id
   name    = var.apps_domain
   type    = "CNAME"
   ttl     = "60"
-  records = ["${aws_lb.cf_system_app_alb.dns_name}"]
+  records = ["${aws_lb.cf_system_app_lb.dns_name}"]
 }
 
 
-output "dns_name" {value = aws_lb.cf_system_app_alb.dns_name}
-output "lb_name"  {value = aws_lb.cf_system_app_alb.name }
-output "lb_target_group_name" { value = aws_lb_target_group.cf_system_app_alb_tg.name }
+output "dns_name" {value = aws_lb.cf_system_app_lb.dns_name}
+output "lb_name"  {value = aws_lb.cf_system_app_lb.name }
+output "lb_target_group_name" { value = aws_lb_target_group.cf_system_app_lb_tg.name }
 
